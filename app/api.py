587701b79cf2ae40df80193ebec76915b4dcf949
riessss <rhouthuijzen@hotmail.com>
2025-07-25 12:23:12 -0400

    Setup basic routing

--- app/api.py
+++ app/api.py
@@ -3,3 +3,38 @@ from flask import (
 )
 
 bp = Blueprint('api', __name__, url_prefix='/api')
+
+
+@bp.route('/song', methods=["GET"])
+def get_song():
+    pass
+
+@bp.route('/song', methods=["POST"])
+def create_song():
+    pass
+
+@bp.route('/song', methods=["PATCH"])
+def update_song():
+    pass
+
+@bp.route('/song', methods=["DELETE"])
+def delete_song():
+    pass
+
+
+
+@bp.route('/instrument', methods=["GET"])
+def get_song():
+    pass
+
+@bp.route('/instrument', methods=["POST"])
+def create_song():
+    pass
+
+@bp.route('/instrument', methods=["PATCH"])
+def update_song():
+    pass
+
+@bp.route('/instrument', methods=["DELETE"])
+def delete_song():
+    pass
---
+++ app/instruments.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+bp = Blueprint('instrument', __name__, url_prefix='/instrument')
+
+@bp.route('/sound')
+def make_sound():
+    duration = 2 # seconds
+    freq = 440.0 # Note A4
+    sample_rate = 44100 # Constant
+
+    t = np.linspace(0, duration, int(sample_rate * duration), False)
+    tone = 0.5 * np.sin(2 * np.pi * freq * t)
+    audio = np.int16(tone * 32767)
+
+    file = write("sound.wav", sample_rate, audio)
+
+    return file
