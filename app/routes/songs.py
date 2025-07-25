757bcda9803bf39def70587f9e8951e41bfe3f5a
riessss <rhouthuijzen@hotmail.com>
2025-07-25 16:24:17 -0400

    Added some routes

--- app/routes/songs.py
+++ app/routes/songs.py
@@ -1,22 +1,67 @@
 from flask import (
-    Blueprint
+    Blueprint,
+    jsonify,
+    request
 )
+
+from ..models import Song, InstrumentLoop
+from ..database import db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-@bp.route('/song', methods=["GET"])
-def get_song():
-    pass
+@bp.route('/song/<int:song_id>', methods=["GET"])
+def get_song(song_id):
+    song = db.get_or_404(Song, song_id)
+    return jsonify({"id": song.id,
+                    "title": song.title,
+                    "tempo": song.tempo,
+                    "instrument_loops": [
+                        {
+                            "id": loop.id, 
+                            "instrument_id": loop.instrument_id,
+                            "notes":[
+                                {
+                                    "id": note.id,
+                                    "pitch": note.pitch,
+                                    "start_time": note.start,
+                                    "duration": note.duration
+                                } for note in loop.notes
+                            ]
+                        } for loop in song.instrument_loops
+                    ]}), 200
 
 @bp.route('/song', methods=["POST"])
 def create_song():
-    pass
+    if request.is_json():
+        data = request.get_json()
+        title = data.get('title')
+        tempo = data.get('tempo')
+        
+        song = Song(title=title, tempo=tempo)
 
-@bp.route('/song', methods=["PATCH"])
-def update_song():
-    pass
+        piano_loop = InstrumentLoop(name="Piano")
+        song.instrument_loops.append(piano_loop)
 
-@bp.route('/song', methods=["DELETE"])
-def delete_song():
-    pass
+        db.session.add(song)
+        db.session.commit()
+
+        return {
+            "id": song.id,
+            "title": song.title,
+            "tempo": song.tempo,
+            "instrument_loops": [
+                {
+                    "id": piano_loop.id,
+                    "instrument_id": piano_loop.instrument_id
+                }
+            ]
+        }, 201
+    
+
+@bp.route('/song/<int:song_id>', methods=["DELETE"])
+def delete_song(song_id):
+    song = Song
+    db.session.delete(song)
+    db.session.commit()
+    return {"message": "Deleting succesfull!"}, 200
